390b21781e76d1435285d59d84a337b2206d66d9
tim <guntertimofej4@gmail.com>
2023-12-01 11:14:37 +0600

    final_test_change

--- Spacebattle.Tests/RotateCommandTest.cs
+++ Spacebattle.Tests/RotateCommandTest.cs
@@ -6,29 +6,48 @@ using Spacebattle.Lib;
 public class RotateCommandTest
 {
     // попытка сдвинуть корабль, у которого невозможно прочитать значение угла наклона к горизонту, приводит к ошибке.
-    [Fact]
-    public void CannotGetAngleThrowsException()
+    [Theory]
+    [InlineData(45)]
+    public void CannotGetAngleThrowsException(int angleVelocity)
     {
         var rotatable = new Mock<IRotatable>();
         rotatable.SetupGet(p => p.HorizonInclinationAngle).Throws(new Exception()).Verifiable();
+        rotatable.SetupGet(r => r.AngularVelocity).Returns(new Angle(angleVelocity)).Verifiable();
+
+        ICommand rotateCommand = new RotateCommand(rotatable.Object);
+
+        Assert.Throws<Exception>(() => rotateCommand.Execute());
     }
 
     // попытка сдвинуть корабль, у которого невозможно изменить угол наклона к горизонту, приводит к ошибке.
     [Theory]
-    [InlineData(45)]
-    public void CannotSetAngleThrowsException(int angle)
+    [InlineData(45, 90)]
+    public void CannotSetAngleThrowsException(int startAngle, int angleVelocity)
     {
         var rotatable = new Mock<IRotatable>();
-        rotatable.SetupSet(x => x.HorizonInclinationAngle = new Angle(angle)).Throws(new Exception());
+        rotatable.SetupGet(r => r.HorizonInclinationAngle).Returns(new Angle(startAngle)).Verifiable();
+        rotatable.SetupGet(r => r.AngularVelocity).Returns(new Angle(angleVelocity)).Verifiable();
+
+        ICommand rotateCommand = new RotateCommand(rotatable.Object);
+        rotatable.SetupSet(x => x.HorizonInclinationAngle = It.IsAny<Angle>()).Throws(new Exception());
+
+        Assert.Throws<Exception>(() => rotateCommand.Execute());
     }
 
     // попытка сдвинуть корабль, у которого невозможно прочитать значение угловой скорости, приводит к ошибке.
 
-    [Fact]
-    public void CannotGetAngularVelocityThrowsException()
+    [Theory]
+    [InlineData(45)]
+    public void CannotGetAngularVelocityThrowsException(int startAngle)
     {
         var rotatable = new Mock<IRotatable>();
+
         rotatable.SetupGet(r => r.AngularVelocity).Throws(new Exception()).Verifiable();
+        rotatable.SetupGet(r => r.HorizonInclinationAngle).Returns(new Angle(startAngle)).Verifiable();
+
+        ICommand rotateCommand = new RotateCommand(rotatable.Object);
+
+        Assert.Throws<Exception>(() => rotateCommand.Execute());
     }
 
     // корабль, который находится под углом к горизонту в 45 градусов имеет угловую скорость 90 градусов. 
