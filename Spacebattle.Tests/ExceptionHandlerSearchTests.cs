eaa3615d075aaceeeabb754ccdb8b985bcdde682
tim <guntertimofej4@gmail.com>
2023-12-28 17:37:36 +0600

    TestCommit

---
+++ Spacebattle.Tests/ExceptionHandlerSearchTests.cs
@@ -0,0 +1,14 @@
+using Hwdtech;
+using Spacebattle.Lib;
+using System;
+using System.Runtime.Serialization;
+using System.Collections.Generic;
+using Moq;
+using System.Diagnostics;
+
+namespace Spacebattle.Tests;
+
+public class ExceptionHandlerSearchTests
+{
+    
+}
