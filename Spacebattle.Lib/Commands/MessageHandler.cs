3b6ae543dbbae7219cdc0219492ffec7e655dba5
xMaksIce <maksiceofficial@gmail.com>
2024-04-26 02:30:01 +0000

    added msgHandler and test

---
+++ Spacebattle.Lib/Commands/MessageHandler.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using Hwdtech;
+
+namespace Spacebattle.Lib;
+
+public class MessageObject
+{
+    public string? Type { get; }
+    public int GameID { get; }
+    public int ItemID { get; }
+    public Hashtable? Parameters { get; }
+}
+public class InterpretMessage : ICommand
+{
+    private readonly MessageObject _messageObj;
+    public InterpretMessage(MessageObject messageObj)
+    {
+        _messageObj = messageObj;
+    }
+    public void Execute()
+    {
+        var command = IoC.Resolve<ICommand>("Game.MessageObjToCommand", _messageObj);
+        IoC.Resolve<ICommand>("Game.PutCommandInQueue", command, _messageObj.GameID).Execute();
+    }
+}
+public class CreateInterpretation : ICommand
+{
+    public void Execute()
+    {
+        var messageObject = IoC.Resolve<MessageObject>("Game.TakeMessageFromQueue");
+        var interpretation = new InterpretMessage(messageObject);
+        IoC.Resolve<ICommand>("Game.PutCommandInQueue", interpretation, messageObject.GameID).Execute();
+    }
+}
