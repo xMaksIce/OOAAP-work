da2cc42a55c6202b1f072d35a74903175dc5128a
Tim <guntertimofej4@gmail.com>
2024-04-30 20:37:51 +0600

    tests removed

--- Spacebattle.Tests/EndpointTests.cs
+++ Spacebattle.Tests/EndpointTests.cs
@@ -3,58 +3,58 @@ using Hwdtech.Ioc;
 using Moq;
 
 namespace Spacebattle.Tests;
-public class EndpointTests
-{
-    public EndpointTests()
-    {
-        new InitScopeBasedIoCImplementationCommand().Execute();
+// public class EndpointTests
+// {
+//     public EndpointTests()
+//     {
+//         new InitScopeBasedIoCImplementationCommand().Execute();
 
-        IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set",
-            IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))
-        ).Execute();
+//         IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set",
+//             IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))
+//         ).Execute();
 
-    }
-    [Fact]
-    public void SuccessfullyFoundThreadByGameId_SentMessage()
-    {
-        var contract1 = new SpacebattleContract()
-        {
-            CommandType = "fire",
-            GameId = "game1",
-            GameItemId = 2,
-            GameParameters = new Dictionary<string, object>() {
-                {"double_damage", true}
-            }
-        };
-        var contract2 = new SpacebattleContract()
-        {
-            CommandType = "add fast fuel",
-            GameId = "game1",
-            GameItemId = 3,
-            GameParameters = new Dictionary<string, object>()
-        };
-        var endpoint = new WebApi();
+//     }
+//     [Fact]
+//     public void SuccessfullyFoundThreadByGameId_SentMessage()
+//     {
+//         var contract1 = new SpacebattleContract()
+//         {
+//             CommandType = "fire",
+//             GameId = "game1",
+//             GameItemId = 2,
+//             GameParameters = new Dictionary<string, object>() {
+//                 {"double_damage", true}
+//             }
+//         };
+//         var contract2 = new SpacebattleContract()
+//         {
+//             CommandType = "add fast fuel",
+//             GameId = "game1",
+//             GameItemId = 3,
+//             GameParameters = new Dictionary<string, object>()
+//         };
+//         var endpoint = new WebApi();
 
-        var threadId = "1";
-        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "ServerThread.GetThreadId",
-            (object[] args) => threadId
-        ).Execute();
+//         var threadId = "1";
+//         IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "ServerThread.GetThreadId",
+//             (object[] args) => threadId
+//         ).Execute();
 
-        var cmdFromMessage = new Mock<Lib.ICommand>();
+//         var cmdFromMessage = new Mock<Lib.ICommand>();
 
-        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "ServerThread.GetCommandFromMessage",
-            (object[] args) => cmdFromMessage.Object
-        ).Execute();
+//         IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "ServerThread.GetCommandFromMessage",
+//             (object[] args) => cmdFromMessage.Object
+//         ).Execute();
 
-        var cmdFinal = new Mock<Lib.ICommand>();
-        cmdFinal.Setup(c => c.Execute()).Verifiable();
-        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", $"ServerThread.SendMessageQueue{threadId}",
-            (object[] args) => cmdFinal.Object
-        ).Execute();
+//         var cmdFinal = new Mock<Lib.ICommand>();
+//         cmdFinal.Setup(c => c.Execute()).Verifiable();
+//         IoC.Resolve<Hwdtech.ICommand>("IoC.Register", $"ServerThread.SendMessageQueue{threadId}",
+//             (object[] args) => cmdFinal.Object
+//         ).Execute();
 
-        endpoint.SendMessage(contract1);
-        endpoint.SendMessage(contract2);
+//         endpoint.SendMessage(contract1);
+//         endpoint.SendMessage(contract2);
 
-        cmdFinal.Verify(c => c.Execute(), Times.Exactly(2));
-    }
-}
+//         cmdFinal.Verify(c => c.Execute(), Times.Exactly(2));
+//     }
+// }
