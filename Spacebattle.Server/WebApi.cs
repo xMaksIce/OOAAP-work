8baab9b1d2b0599ee0076ac5b80896ec77f631e0
Tim <guntertimofej4@gmail.com>
2024-04-30 23:15:07 +0600

    Tests added

--- Spacebattle.Server/WebApi.cs
+++ Spacebattle.Server/WebApi.cs
@@ -8,7 +8,6 @@ namespace Spacebattle.Server
     {
         public void SendMessage(SpacebattleContract contract)
         {
-
             var threadId = IoC.Resolve<string>("ServerThread.GetThreadId", contract.GameId);
 
             var cmdFromMessage = IoC.Resolve<Lib.ICommand>("ServerThread.GetCommandFromMessage", contract);
--- Spacebattle.Tests/EndpointTests.cs
+++ Spacebattle.Tests/EndpointTests.cs
@@ -57,4 +57,126 @@ public class EndpointTests
 
         cmdFinal.Verify(c => c.Execute(), Times.Exactly(2));
     }
+
+    [Fact]
+    public void FailedToFindThreadId()
+    {
+        var contract1 = new SpacebattleContract()
+        {
+            CommandType = "fire",
+            GameId = "id2",
+            GameItemId = 2,
+            GameParameters = new Dictionary<string, object>() {
+                {"double_damage", true}
+            }
+        };
+
+        var endpoint = new WebApi();
+
+        var threadId = "1";
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "ServerThread.GetThreadId",
+            (object[] args) =>
+            {
+                return (object)new Exception();
+            }
+        ).Execute();
+
+        var cmdFromMessage = new Mock<Lib.ICommand>();
+
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "ServerThread.GetCommandFromMessage",
+            (object[] args) => cmdFromMessage.Object
+        ).Execute();
+
+        var cmdFinal = new Mock<Lib.ICommand>();
+        cmdFinal.Setup(c => c.Execute()).Verifiable();
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", $"ServerThread.SendMessageQueue{threadId}",
+            (object[] args) => cmdFinal.Object
+        ).Execute();
+
+        Assert.Throws<InvalidCastException>(() => endpoint.SendMessage(contract1));
+
+    }
+
+    [Fact]
+    public void FailedToExecuteQueueFinalCommand()
+    {
+        var contract = new SpacebattleContract()
+        {
+            CommandType = "fire",
+            GameId = "3",
+            GameItemId = 2,
+            GameParameters = new Dictionary<string, object>() {
+                {"double_damage", true}
+            }
+        };
+
+        var endpoint = new WebApi();
+
+        var threadId = "1";
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "ServerThread.GetThreadId",
+            (object[] args) =>
+            {
+                return threadId;
+            }
+        ).Execute();
+
+        var cmdFromMessage = new Mock<Lib.ICommand>();
+
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "ServerThread.GetCommandFromMessage",
+            (object[] args) => cmdFromMessage.Object
+        ).Execute();
+
+        var cmdFinal = new Mock<Lib.ICommand>();
+        cmdFinal.Setup(c => c.Execute()).Throws(new Exception());
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", $"ServerThread.SendMessageQueue{threadId}",
+            (object[] args) => cmdFinal.Object
+        ).Execute();
+
+        Assert.Throws<Exception>(() => endpoint.SendMessage(contract));
+
+    }
+
+    [Fact]
+    public void FailedToCallMessageCommand()
+    {
+        var contract = new SpacebattleContract()
+        {
+            CommandType = "fire",
+            GameId = "3",
+            GameItemId = 2,
+            GameParameters = new Dictionary<string, object>() {
+                {"double_damage", true}
+            }
+        };
+
+        var endpoint = new WebApi();
+
+        var threadId = "1";
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "ServerThread.GetThreadId",
+            (object[] args) =>
+            {
+                return threadId;
+            }
+        ).Execute();
+
+        var exCmd = new Mock<Lib.ICommand>();
+        exCmd.Setup(c => c.Execute()).Throws(new Exception());
+        var cmdFromMessage = new Mock<Lib.ICommand>();
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "ServerThread.GetCommandFromMessage",
+            (object[] args) =>
+            {
+                exCmd.Object.Execute();
+                return cmdFromMessage.Object;
+            }
+        ).Execute();
+
+        var cmdFinal = new Mock<Lib.ICommand>();
+        cmdFinal.Setup(c => c.Execute()).Throws(new Exception());
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", $"ServerThread.SendMessageQueue{threadId}",
+            (object[] args) => cmdFinal.Object
+        ).Execute();
+
+        Assert.Throws<Exception>(() => endpoint.SendMessage(contract));
+
+    }
 }
