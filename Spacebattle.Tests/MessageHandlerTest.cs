b5905d5f5c7f0426465ef7e0d2882006f819072b
xMaksIce <maksiceofficial@gmail.com>
2024-04-30 04:02:04 +0000

    New tests

--- Spacebattle.Tests/MessageHandlerTest.cs
+++ Spacebattle.Tests/MessageHandlerTest.cs
@@ -6,12 +6,14 @@ namespace Spacebattle.Tests;
 
 public class MessageHandlerTest
 {
-    [Fact]
-    public void CommandAssemblesFromObjThenExecutes()
+    public MessageHandlerTest()
     {
         new InitScopeBasedIoCImplementationCommand().Execute();
         IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))).Execute();
-
+    }
+    [Fact]
+    public void CommandAssemblesFromObjThenExecutes()
+    {
         var command = new Mock<Lib.ICommand>();
         command.Setup(cmd => cmd.Execute()).Verifiable();
         IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.MessageObjToCommand", (object[] args) => command.Object).Execute();
@@ -23,5 +25,36 @@ public class MessageHandlerTest
 
         new CreateInterpretation().Execute();
         command.Verify(cmd => cmd.Execute(), Times.Once);
+    }
+    [Fact]
+    public void InvalidGameID()
+    {
+        var command = new Mock<Lib.ICommand>();
+        command.Setup(cmd => cmd.Execute()).Verifiable();
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.MessageObjToCommand", (object[] args) => command.Object).Execute();
+
+        var messageObj = new Mock<MessageObject>();
+        messageObj.SetupGet(msg => msg.GameID).Throws(new ArgumentException()).Verifiable();
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.TakeMessageFromQueue", (object[] args) => messageObj.Object).Execute();
+
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.PutCommandInQueue", (object[] args) => args[0]).Execute();
+
+        var createInterpretation = new CreateInterpretation();
+        Assert.Throws<ArgumentException>(createInterpretation.Execute);
+    }
+    [Fact]
+    public void InvalidCommand()
+    {
+        var command = new Mock<Lib.ICommand>();
+        command.Setup(cmd => cmd.Execute()).Throws(new Exception()).Verifiable();
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.MessageObjToCommand", (object[] args) => command.Object).Execute();
+
+        var messageObj = new Mock<MessageObject>();
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.TakeMessageFromQueue", (object[] args) => messageObj.Object).Execute();
+
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.PutCommandInQueue", (object[] args) => args[0]).Execute();
+
+        var createInterpretation = new CreateInterpretation();
+        Assert.Throws<Exception>(createInterpretation.Execute);
     }
 }
