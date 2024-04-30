d4250d7ed354d26d70d4fd1e5085690fdcd3e24d
Tim <guntertimofej4@gmail.com>
2024-04-30 20:25:36 +0600

    seems to be ready

---
+++ Spacebattle.Lib/SpacebattleContract.cs
@@ -0,0 +1,28 @@
+
+using System.Runtime.Serialization;
+using CoreWCF.OpenApi.Attributes;
+
+namespace Spacebattle.Lib
+{
+    [DataContract(Name = "SpacebattleContract")]
+    public class SpacebattleContract
+    {
+
+        [DataMember(Name = "type", Order = 1)]
+        [OpenApiProperty(Description = "Command type description.")]
+        public required string CommandType { get; set; }
+
+        [DataMember(Name = "game id", Order = 2)]
+        [OpenApiProperty(Description = "Game id description.")]
+        public required string GameId { get; set; }
+
+        [DataMember(Name = "game item id", Order = 3)]
+        [OpenApiProperty(Description = "Game item id description.")]
+        public required int GameItemId { get; set; }
+
+        [DataMember(Name = "params", Order = 4)]
+        [OpenApiProperty(Description = "Params description.")]
+        public required IDictionary<string, object> GameParameters { get; set; }
+    }
+    
+}
---
+++ Spacebattle.Server/WebApi.cs
@@ -0,0 +1,18 @@
+using Hwdtech;
+using CoreWCF;
+
+namespace Spacebattle.Server
+{
+    [ServiceBehavior(IncludeExceptionDetailInFaults = true)]
+    public class WebApi : IWebApi
+    {
+        public void SendMessage(SpacebattleContract contract) {
+        
+            var threadId = IoC.Resolve<string>("ServerThread.GetThreadId", contract.GameId);
+            
+            var cmdFromMessage = IoC.Resolve<Lib.ICommand>("ServerThread.GetCommandFromMessage", contract);
+
+            IoC.Resolve<Lib.ICommand>($"ServerThread.SendMessageQueue{threadId}", contract, cmdFromMessage).Execute();
+        }
+    }
+}
